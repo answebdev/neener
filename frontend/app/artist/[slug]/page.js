52e53c758e413a99b166e75fd762fff84a7d661a
Adolf Schmuck <britvaica@netscape.net>
2025-06-21 10:24:02 -0700

    adjust padding

--- frontend/app/artist/[slug]/page.js
+++ frontend/app/artist/[slug]/page.js
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic'; // Enable SSR
 
 import { PortableText } from '@portabletext/react';
 import client from '../../../client';
