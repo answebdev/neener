5089aac80b65c49e471c42bb1a74035309d7e1c3
Adolf Schmuck <britvaica@netscape.net>
2025-10-21 20:07:44 -0700

    update configuration of sanity.cli.js

--- backend/sanity.cli.js
+++ backend/sanity.cli.js
@@ -4,11 +4,13 @@ export default defineCliConfig({
   api: {
     projectId: 'wfwums7k',
     dataset: 'production',
-    appId: 'ps9q8gyxk9eoa14eeyz4l6s1',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  deployment: {
+    appId: 'ps9q8gyxk9eoa14eeyz4l6s1',
+  },
 })
