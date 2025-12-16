bd7d7433353c5074b9aa45a06b8de6cd17ae243f
Adolf Schmuck <britvaica@netscape.net>
2025-12-16 14:47:13 -0800

    add portable text component

--- frontend/app/artist/[slug]/page.js
+++ frontend/app/artist/[slug]/page.js
@@ -5,79 +5,97 @@ import client from '../../../client';
 import Image from 'next/image';
 import Link from 'next/link';
 import { urlFor } from '../../../lib/sanityImageUrl';
-import { portableTextComponents } from '@/components/portableTextComponents';
 import styles from './ArtistPage.module.css';
 
 // Define how Portable Text should render custom types like images
-// const ptComponents = {
-//   types: {
-//     image: ({ value }) => {
-//       if (!value?.asset?._ref) {
-//         return null;
-//       }
-//       return (
-//         // eslint-disable-next-line @next/next/no-img-element
-//         <img
-//           alt={value.alt || ' '}
-//           loading='lazy'
-//           src={urlFor(value)
-//             .width(400)
-//             .height(400)
-//             .fit('max')
-//             .auto('format')
-//             .url()}
-//           style={{ margin: '1em 0', maxWidth: '100%' }}
-//         />
-//       );
-//     },
-//     iframeEmbed: ({ value }) => {
-//       return (
-//         <div style={{ maxWidth: '700px', margin: '2em 0', width: '100%' }}>
-//           <iframe
-//             className={styles.desktopPlayer}
-//             src={value.url}
-//             style={{
-//               border: 0,
-//               width: '100%',
-//               height: '120px',
-//             }}
-//             loading='lazy'
-//             seamless
-//           />
-//           <iframe
-//             className={styles.mobilePlayer}
-//             src={value.url.replace('size=large', 'size=medium')}
-//             style={{
-//               width: '100%',
-//               height: '120px',
-//               border: '0',
-//             }}
-//             loading='lazy'
-//             seamless
-//           />
-//         </div>
-//       );
-//     },
-//     audioEmbed: ({ value }) => (
-//       <div>
-//         {value.audioUrl && (
-//           <>
-//             <p style={{ fontWeight: 'bold', textAlign: 'center' }}>
-//               {value.title}
-//             </p>
-//             {value.artistName && (
-//               <p style={{ textAlign: 'center' }}>by {value.artistName}</p>
-//             )}
-//             <audio controls style={{ width: '100%' }} controlsList='nodownload'>
-//               <source src={value.audioUrl} type='audio/mpeg' />
-//               Your browser does not support the audio element.
-//             </audio>
-//           </>
-//         )}
-//       </div>
-//     ),
-//   },
-// };
+const ptComponents = {
+  types: {
+    image: ({ value }) => {
+      if (!value?.asset?._ref) {
+        return null;
+      }
+      return (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          alt={value.alt || ' '}
+          loading='lazy'
+          src={urlFor(value)
+            .width(400)
+            .height(400)
+            .fit('max')
+            .auto('format')
+            .url()}
+          style={{ margin: '1em 0', maxWidth: '100%' }}
+        />
+      );
+    },
+    iframeEmbed: ({ value }) => {
+      return (
+        // Player Not Centered:
+        <div style={{ maxWidth: '700px', margin: '2em 0', width: '100%' }}>
+          {/* // Player Centered: */}
+          {/* <div style={{ maxWidth: '700px', margin: '2em auto', width: '100%' }}> */}
+          <iframe
+            className={styles.desktopPlayer}
+            src={value.url}
+            style={{
+              border: 0,
+              width: '100%',
+              height: '120px',
+            }}
+            loading='lazy'
+            seamless
+          />
+          <iframe
+            className={styles.mobilePlayer}
+            src={value.url.replace('size=large', 'size=medium')}
+            style={{
+              width: '100%',
+              height: '120px',
+              border: '0',
+            }}
+            loading='lazy'
+            seamless
+          />
+        </div>
+      );
+    },
+    audioEmbed: ({ value }) => (
+      <div>
+        {value.audioUrl && (
+          <>
+            <p style={{ fontWeight: 'bold', textAlign: 'center' }}>
+              {value.title}
+            </p>
+            {value.artistName && (
+              <p style={{ textAlign: 'center' }}>by {value.artistName}</p>
+            )}
+            {/* <audio controls controlsList='nodownload'> */}
+            <audio controls style={{ width: '100%' }} controlsList='nodownload'>
+              <source src={value.audioUrl} type='audio/mpeg' />
+              Your browser does not support the audio element.
+            </audio>
+          </>
+        )}
+      </div>
+    ),
+  },
+  marks: {
+    link: ({ value, children }) => {
+      const { href, blank } = value;
+      return (
+        <a
+          href={href}
+          target={blank ? '_blank' : '_self'}
+          rel={blank ? 'noopener noreferrer' : undefined}
+          style={{ color: 'blue', textDecoration: 'underline' }}
+        >
+          {children}
+        </a>
+      );
+    },
+  },
+};
 
 export async function generateMetadata({ params }) {
   const { slug } = await params;
@@ -220,7 +238,7 @@ const ArtistPage = async (props) => {
           </ul>
         )}
 
-        <PortableText value={artist.body} components={portableTextComponents} />
+        <PortableText value={artist.body} components={ptComponents} />
 
         {/* <p style={{ marginTop: '2em' }}> */}
         <p className={styles.backButton}>
